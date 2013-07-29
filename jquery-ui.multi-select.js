62fe753651e125524109517874247c668fa69c3b
Jeremie Jarosh <jeremiej@ecreativeworks.com>
2013-07-29 16:21:14 -0500

    clean-up code
    - clean up useless code
    
    - remove PHP from the example

--- jquery-ui.multi-select.js
+++ jquery-ui.multi-select.js
@@ -22,16 +22,13 @@
 			this.toLeftBtn    = $('<button/>',{text:this.options.toLeftText}).addClass("move-to-left");
 			this.toRightBtn   = $('<button/>',{text:this.options.toRightText}).addClass("move-to-right");
 
-
-			//this.wrapper.addClass("ui-multi-select");
 			this.element.wrap( this.wrapper );
 			this.element.hide();
 
-			this.wrapper = this.element.parent();
+			this.wrapper = this.element.parent();  // make sure wrapper is pointing to the correct element
 
 			this.buttonGrp.append( this.toRightBtn );
 			this.buttonGrp.append( this.toLeftBtn );
-			//buttonGrp.appendTo( wrapper );
 
 			this.toLeftBtn.button();
 			this.toRightBtn.button();
@@ -52,18 +49,8 @@
 			console.log('refresh');
 
 			var thisWidget = this;
-			//var main  = this.element;
 			var leftElm   = this.leftElement;
 			var rightElm  = this.rightElement;
-			/*var removeOpt = function($element, value, text) {
-				$element.find('option').each(function(idx,elm) {
-					var elmValue = $(elm).attr('value');
-					var elmText  = $(elm).text;
-					if ( elmValue == value && elmText == text ) {
-						$(elm).remove();
-					}
-				});
-			};*/
 
 			leftElm.children().remove();
 			leftElm.append( this.element.children().clone() );
@@ -73,36 +60,21 @@
 			rightElm.append( this.element.children().clone() );
 			rightElm.find('option:selected').prop('selected',false);
 
-			//console.log('boo');
-
 			this.element.find('option').each(function(idx,elm) {
 				var elmValue = $(elm).attr('value');
-				//var elmText  = $(elm).text;
 				var selected = $(elm).prop('selected');
-				//console.log(elm, selected);
 
 				if ( selected ) {
-					//removeOpt( leftElm, elmValue, elmText );
 					thisWidget.simularOption( elm, leftElm ).remove();
 				} else {
-					//removeOpt( rightElm, elmValue, elmText );
 					thisWidget.simularOption( elm, rightElm ).remove();
 				}
 
 			});
 
-
 			// trigger a callback/event
 			this._trigger( "change" );
 		},
-
-/*
-		// a public method to change the color to a random value
-		// can be called directly via .colorize( "random" )
-		random: function( event ) {
-
-		},
-*/
 
 		// events bound via _on are removed automatically
 		// revert other modifications here
@@ -111,21 +83,11 @@
 			this.leftElement.remove();
 			this.buttonGrp.remove();
 			this.rightElement.remove();
-			//this.element.parent().children('.select-right, .select-left, .button-group').remove();
 
 			this.element.unwrap();
 			this.element.show();
 		},
-
-/*
-		// _setOptions is called with a hash of all options that are changing
-		// always refresh when changing options
-		_setOptions: function() {
-			// _super and _superApply handle keeping the right this-context
-			this._superApply( arguments );
-			this._refresh();
-		},
-*/
+		
 		// _setOption is called for each individual option that is changing
 		_setOption: function( key, value ) {
 			if ( key == "toLeftText" ) this.toLeftBtn.button('option', 'label', value);
@@ -133,20 +95,16 @@
 			this._super( key, value );
 		},
 
-
+		// find simular <option> element in given <select> element
 		simularOption: function( optionElm, selectElm ) {
 			selectElm = selectElm || this.element;
 
 			var $found   = $([]);
 			var optValue = $(optionElm).attr('value');
-			//var optText  = $(optionElm).text();
-			//	console.log("look",optionElm,optValue,optText);
 
 			$(selectElm).find('option').each(function(idx,elm) {
 				var elmValue = $(elm).attr('value');
-				//var elmText  = $(elm).text();
-				//console.log("see",elm,elmValue,elmText);
-				if ( elmValue == optValue/* && elmText == optText */) {
+				if ( elmValue == optValue ) {
 					$found = $(elm);
 					return false;
 				}
@@ -155,6 +113,7 @@
 			return $found;
 		},
 
+		// move selected <option>s from the right to the left
 		moveToLeft: function() {
 			console.log("move to left");
 			var thisWidget = this;
@@ -165,6 +124,7 @@
 			return false;
 		},
 
+		// move selected <option>s from the left to the right
 		moveToRight: function() {
 			console.log("move to right");
 			var thisWidget = this;
