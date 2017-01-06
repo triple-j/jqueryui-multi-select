31b10a0632488f86d4ce7e46002160d2e1b609b7
Jeremie Jarosh <jjarosh@reachmedianetwork.com>
2017-01-06 15:59:46 -0600

    Convert Tabs to Spaces

--- jquery-ui.multi-select.js
+++ jquery-ui.multi-select.js
@@ -1,131 +1,131 @@
 (function( $, window, undefined ) {
 
-	"use strict";
+    "use strict";
 
-	$.widget( "custom.multiselect", {
+    $.widget( "custom.multiselect", {
 
-		// default options
-		options: {
-			toLeftText:  "<<",
-			toRightText: ">>",
+        // default options
+        options: {
+            toLeftText:  "<<",
+            toRightText: ">>",
 
-			// callbacks
-			change: null
-		},
+            // callbacks
+            change: null
+        },
 
-		// the constructor
-		_create: function() {
-			this.wrapper      = $('<span/>').addClass("ui-multi-select");
-			this.leftElement  = $('<select/>',{multiple:true}).addClass("select-left");
-			this.rightElement = $('<select/>',{multiple:true}).addClass("select-right");
-			this.buttonGrp    = $('<span/>').addClass("button-group");
-			this.toLeftBtn    = $('<button/>',{text:this.options.toLeftText}).addClass("move-to-left");
-			this.toRightBtn   = $('<button/>',{text:this.options.toRightText}).addClass("move-to-right");
+        // the constructor
+        _create: function() {
+            this.wrapper      = $('<span/>').addClass("ui-multi-select");
+            this.leftElement  = $('<select/>',{multiple:true}).addClass("select-left");
+            this.rightElement = $('<select/>',{multiple:true}).addClass("select-right");
+            this.buttonGrp    = $('<span/>').addClass("button-group");
+            this.toLeftBtn    = $('<button/>',{text:this.options.toLeftText}).addClass("move-to-left");
+            this.toRightBtn   = $('<button/>',{text:this.options.toRightText}).addClass("move-to-right");
 
-			this.element.wrap( this.wrapper );
-			this.element.hide();
+            this.element.wrap( this.wrapper );
+            this.element.hide();
 
-			this.wrapper = this.element.parent();  // make sure wrapper is pointing to the correct element
+            this.wrapper = this.element.parent();  // make sure wrapper is pointing to the correct element
 
-			this.buttonGrp.append( this.toRightBtn );
-			this.buttonGrp.append( this.toLeftBtn );
+            this.buttonGrp.append( this.toRightBtn );
+            this.buttonGrp.append( this.toLeftBtn );
 
-			this.toLeftBtn.button();
-			this.toRightBtn.button();
+            this.toLeftBtn.button();
+            this.toRightBtn.button();
 
-			this.wrapper.append( this.leftElement );
-			this.wrapper.append( this.buttonGrp );
-			this.wrapper.append( this.rightElement );
+            this.wrapper.append( this.leftElement );
+            this.wrapper.append( this.buttonGrp );
+            this.wrapper.append( this.rightElement );
 
-			this._on(this.toLeftBtn, {click:"moveToLeft"});
-			this._on(this.toRightBtn, {click:"moveToRight"});
-			this._on(this.element, {change:"_refresh"});
+            this._on(this.toLeftBtn, {click:"moveToLeft"});
+            this._on(this.toRightBtn, {click:"moveToRight"});
+            this._on(this.element, {change:"_refresh"});
 
-			this._refresh( false );
-		},
+            this._refresh( false );
+        },
 
-		// called when created, and later when changing options
-		_refresh: function( triggerChange ) {
-			triggerChange = (typeof triggerChange === "undefined") ? true : triggerChange;
-			
-			var thisWidget = this,
-			    leftElm    = this.leftElement,
-			    rightElm   = this.rightElement;
+        // called when created, and later when changing options
+        _refresh: function( triggerChange ) {
+            triggerChange = (typeof triggerChange === "undefined") ? true : triggerChange;
+            
+            var thisWidget = this,
+                leftElm    = this.leftElement,
+                rightElm   = this.rightElement;
 
-			leftElm.children().remove();
-			leftElm.append( this.element.children().clone() );
-			leftElm.find('option:selected').prop('selected',false);
+            leftElm.children().remove();
+            leftElm.append( this.element.children().clone() );
+            leftElm.find('option:selected').prop('selected',false);
 
-			rightElm.children().remove();
-			rightElm.append( this.element.children().clone() );
-			rightElm.find('option:selected').prop('selected',false);
+            rightElm.children().remove();
+            rightElm.append( this.element.children().clone() );
+            rightElm.find('option:selected').prop('selected',false);
 
-			this.element.find('option').each(function(idx,elm) {
-				var selected = $(elm).prop('selected');
-				thisWidget.simularOption( elm, (selected ? leftElm : rightElm) ).remove();
-			});
+            this.element.find('option').each(function(idx,elm) {
+                var selected = $(elm).prop('selected');
+                thisWidget.simularOption( elm, (selected ? leftElm : rightElm) ).remove();
+            });
 
-			// trigger a callback/event
-			if ( triggerChange ) { this._trigger( "change" ); }
-		},
+            // trigger a callback/event
+            if ( triggerChange ) { this._trigger( "change" ); }
+        },
 
-		// events bound via _on are removed automatically
-		// revert other modifications here
-		_destroy: function() {
-			// remove generated elements
-			this.leftElement.remove();
-			this.buttonGrp.remove();
-			this.rightElement.remove();
+        // events bound via _on are removed automatically
+        // revert other modifications here
+        _destroy: function() {
+            // remove generated elements
+            this.leftElement.remove();
+            this.buttonGrp.remove();
+            this.rightElement.remove();
 
-			this.element.unwrap();
-			this.element.show();
-		},
-		
-		// _setOption is called for each individual option that is changing
-		_setOption: function( key, value ) {
-			if ( key == "toLeftText" ) { this.toLeftBtn.button('option', 'label', value); }
-			if ( key == "toRightText" ) { this.toRightBtn.button('option', 'label', value); }
-			this._super( key, value );
-		},
+            this.element.unwrap();
+            this.element.show();
+        },
+        
+        // _setOption is called for each individual option that is changing
+        _setOption: function( key, value ) {
+            if ( key == "toLeftText" ) { this.toLeftBtn.button('option', 'label', value); }
+            if ( key == "toRightText" ) { this.toRightBtn.button('option', 'label', value); }
+            this._super( key, value );
+        },
 
-		// find simular <option> element in given <select> element
-		simularOption: function( optionElm, selectElm ) {
-			selectElm = selectElm || this.element;
+        // find simular <option> element in given <select> element
+        simularOption: function( optionElm, selectElm ) {
+            selectElm = selectElm || this.element;
 
-			var $found   = $([]),
-			    optValue = $(optionElm).attr('value');
+            var $found   = $([]),
+                optValue = $(optionElm).attr('value');
 
-			$(selectElm).find('option').each(function(idx,elm) {
-				var elmValue = $(elm).attr('value');
-				if ( elmValue == optValue ) {
-					$found = $(elm);
-					return false;
-				}
-			});
+            $(selectElm).find('option').each(function(idx,elm) {
+                var elmValue = $(elm).attr('value');
+                if ( elmValue == optValue ) {
+                    $found = $(elm);
+                    return false;
+                }
+            });
 
-			return $found;
-		},
+            return $found;
+        },
 
-		// move selected <option>s from the right to the left
-		moveToLeft: function() {
-			var thisWidget = this;
-			this.rightElement.find('option:selected').each(function(idx,elm) {
-				thisWidget.simularOption(elm).prop('selected',false);
-			});
-			this._refresh();
-			return false;
-		},
+        // move selected <option>s from the right to the left
+        moveToLeft: function() {
+            var thisWidget = this;
+            this.rightElement.find('option:selected').each(function(idx,elm) {
+                thisWidget.simularOption(elm).prop('selected',false);
+            });
+            this._refresh();
+            return false;
+        },
 
-		// move selected <option>s from the left to the right
-		moveToRight: function() {
-			var thisWidget = this;
-			this.leftElement.find('option:selected').each(function(idx,elm) {
-				thisWidget.simularOption(elm).prop('selected',true);
-			});
-			this._refresh();
-			return false;
-		}
+        // move selected <option>s from the left to the right
+        moveToRight: function() {
+            var thisWidget = this;
+            this.leftElement.find('option:selected').each(function(idx,elm) {
+                thisWidget.simularOption(elm).prop('selected',true);
+            });
+            this._refresh();
+            return false;
+        }
 
-	});
+    });
 
 })( jQuery, window );
